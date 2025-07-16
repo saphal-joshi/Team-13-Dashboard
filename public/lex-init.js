bccdf62147b61b5afc02c5cdab266229f76157b0
Lee-Phoenix <lee_phoenix@bah.com>
2025-07-16 14:52:16 -0400

    first commit

--- public/lex-init.js
+++ public/lex-init.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   AWS.config.region = "us-east-1";
   AWS.config.credentials = new AWS.Credentials({
-    accessKeyId: "ASIA27PR364I3OX3VLTI",
-    secretAccessKey: "1d7WuxmM/sdgrqjfe6Io+afW99kG4ZJE3vo3tKIO",
-    sessionToken: "IQoJb3JpZ2luX2VjEEkaCXVzLWVhc3QtMSJIMEYCIQCjcPDkhCFuKhYVxFjHF+8QK6OYkBZu6Jc4OXACM2r1FQIhANcS+eUNadT/kC1XF4h8KfULFYbK9WvTGtwdL+65lLOzKvkCCGIQABoMNzU0ODEwODc3NzEzIgxrc41rJbjFCaIgA+Uq1gKg5tmHP8BIPRoT8xPy39jv65x04Y1TxdHpqs+emCWS5xCjWlIdybvIdSy5i317sAikuW2EUBwcySd+KtJS7y8szb7qtwdO6cbExRDaoIFyc4r4LwdlyXUSzdNeXFNL+f6KhutuVBrIy6fdxLnPPVojOILxCoD4ALG4MLNqrIG4ItoZg/eJwPsdiaV2QNXeWGzBwol6ygXTZPe4F/BBSG52lMiTJWp4MbZvvq44hXqgzVWCWjaLHffhugdLPdVzFx/c5Sxm+H/KAi1em0wPIvDWrJ4pLFS2R22fVcGMuylYI6yvgGLfJuNemRQAJBjC9wC85h1IYgansYiHqyU6/EaXRe1VXn5ujdz1KBUW63X86gO6sijGgtoaGGREyFgoOqI6v98fWwH8JdNEVomC6IlshdDYtBO8gE/vZglSw3lj9D5rv52Mb44Y+3+QPGBRQgcFvCay3qMwgrDfwwY6pgGBvWhBzhrY3PrwKBUuaFDY7en0y9/JvbkKzxHZtNvQZO6mjql/i++W3mftpdxbE8QqA05AuDsL3u/5OmxoZjP2n7NdUYUk9UoLcAkYrMQCWeU/X5HjYeBI2jVD8eJrFGCt9VhMDWvj6VM0bKfFCLl3p5Gb6oKoCmGc0KzzerdzYgq5ZuVTS24rGxQtn0rw8zggDKBndwZ42VbILl98uuodjT6sBnX7"
+    accessKeyId: "",
+    secretAccessKey: "",
+    sessionToken: ""
   });
  
   const lexruntimev2 = new AWS.LexRuntimeV2();
@@ -35,8 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
     chatDiv.innerHTML += `<p><strong>You:</strong> ${message}</p>`;
  
     const params = {
-      botAliasId: "XL3HUOOBXE", // e.g. "TSTALIASID"
-      botId: "J04J53KVTF",        
+      botAliasId: "", // e.g. "TSTALIASID"
+      botId: "",        
       localeId: "en_US",                // or your bot locale
       sessionId: sessionId,
       text: message
